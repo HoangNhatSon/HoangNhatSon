44e1159c48f84ca442ccb3d38cb8699eea981cc7
SonHoangNhat <SonGonkilua113@gmail.com>
2017-04-11 10:48:59 +0700

    module_11_4
    up module

---
+++ module-4/index.php
@@ -0,0 +1,43 @@
+
+<html>
+    <head>
+        
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="bootstrap-3.3.6-dist/js/bootstrap.min.js" type="text/javascript"></script>
+        <link href="font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/1.css" rel="stylesheet" type="text/css" />
+        
+    </head>
+
+    <body>
+      
+        <div class ="module-4">
+            <div class="fullsize">
+                
+                    <div class="fullsize1">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h5>TASTY AND CRUNCHY</h5>
+                                <h2>INGREDIENTS</h2>
+                                <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod<br>
+                                    tempor incidition ullamco laboris nisi ut aliquip ex ea commodo condor <br>
+                                    consectetur adipiscing elit, sed do eiusmod tempor incidition ullam.
+                                </h4>
+                                <h3><a href="#" >VIEW OUT MENU</a></h3> 
+                            </div>
+                            <div class="col-md-6">
+                                <img src="images/home1-main-image-4.jpg" alt=""/>
+                            </div>
+                        </div>
+                        
+                    </div>
+              
+               
+            </div>
+           
+        </div>
+     <div class="clearfix"></div>
+    </body>
+   <div class="clearfix"></div>
+</html>
